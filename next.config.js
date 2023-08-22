56226cef6f94b4022ce0940db79ae2dfc38ca10f
momota khatun <momotakhatun014@gmail.com>
2023-08-22 21:21:39 +0600

    fix issue

--- next.config.js
+++ next.config.js
@@ -1,9 +1,29 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   images: {
     unoptimized: true,
   },
+  headers: [
+    {
+      key: "X-Frame-Options",
+      value: "DENY",
+    },
+  ],
   distDir: "build",
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
